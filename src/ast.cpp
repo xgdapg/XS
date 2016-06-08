0b57f1acc8d0cfef3f0da4de9aa247bc1d29aa1a
xgdapg <xgdapg@gmail.com>
2016-06-08 14:06:13 +0800

    update

--- src/ast.cpp
+++ src/ast.cpp
@@ -1,14 +1,60 @@
 #include "ast.h"
 
 namespace Lang {
+	Token * AST::token(unsigned int i) {
+		i += index;
+		if (i < tokens.size()) return tokens[i];
+		return emptyToken;
+	}
+
 	void AST::buildWithTokens(vector<Token*> tokens) {
-		unsigned int i = 0;
-		while (i < tokens.size()) {
-			auto token = tokens[i];
-			
-			if (token->type == Token::Token::tDeclareVar) {
-				
+		while (token() != emptyToken) {
+			if (token()->type == Token::Type::tDeclareVar) {
+				Root->addChild(createDeclareVar());
 			}
+			if (token()->type == Token::Type::tAssign) {
+				Root->addChild(createAssign());
+			}
+		}
+	}
+
+	AST::Node* AST::createDeclareVar() {
+		auto node = new Node(token());
+		index++;
+		if (token()->kind != Token::Kind::kIdentifier) throw exception("require identifier");
+		node->addChild(new Node(token()));
+		index++;
+		if (token()->type == Token::Type::tSemicolon && token(1)->kind == Token::Kind::kIdentifier) {
+			node->addChild(new Node(token(1)));
+			index += 2;
+		} else {
+			node->addChild(new Node(new Token(Token::Kind::kIdentifier, Token::Type::tVarType, "unknown", 0, 0)));
+		}
+		return node;
+	}
+
+	AST::Node* AST::createAssign() {
+		auto node = new Node(token());
+		index++;
+
+	}
+
+	AST::Node* AST::createExpr() {
+		Node* node = nullptr;
+		auto t = token();
+		if (t->kind == Token::Kind::kLiteral || t->kind == Token::Kind::kIdentifier) {
+			if (token(1)->kind == Token::Kind::kOperator) {
+				node = new Node(token(1));
+				node->addChild(new Node(t));
+				index += 2;
+				node->addChild(createExpr());
+			} else {
+				node = new Node(t);
+				index += 1;
+			}
+		}
+		else if (t->type == Token::Type::tLParen) {
+
 		}
 	}
 }
--- src/ast.h
+++ src/ast.h
@@ -11,11 +11,11 @@ namespace Lang {
 	public:
 		class Node {
 		public:
-			Token* token;
+			Token* token = nullptr;
 			vector<Node*> children;
+			Node* parent = nullptr;
 
 			Node() {
-				token = nullptr;
 				children = vector<Node*>();
 			}
 
@@ -23,14 +23,32 @@ namespace Lang {
 				token = t;
 				children = vector<Node*>();
 			}
+
+			void addChild(Node* node) {
+				children.push_back(node);
+				if (node->parent == nullptr) node->parent = this;
+			}
 		};
 
 		Node* Root;
-		AST() {
+		vector<Token*> tokens;
+		unsigned int index;
+		Token* emptyToken;
+
+		AST(vector<Token*> ts) {
 			Root = new Node();
+			tokens = ts;
+			index = 0;
+			emptyToken = new Token(Token::Kind::kUnknown, Token::Type::tUnknown, "", 0, 0);
 		}
 
+		Token* token(unsigned int i = 0);
+
 		void buildWithTokens(vector<Token*> tokens);
+
+		Node* createDeclareVar();
+		Node* createAssign();
+		Node* createExpr();
 	};
 	
 
